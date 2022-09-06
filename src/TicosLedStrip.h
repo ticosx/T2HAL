79081dedcf140b72f855da0a05394815eafc21d9
Masa <20962057+hypermasa@users.noreply.github.com>
2022-09-06 12:40:27 +0800

    feat: 去除hsv2rgb转换函数

--- src/TicosLedStrip.h
+++ src/TicosLedStrip.h
@@ -9,17 +9,10 @@ typedef struct {
     uint8_t b;
 } TicosRGB888;
 
-typedef struct {
-    uint8_t h;
-    uint8_t s;
-    uint8_t v;
-} TicosHSV888;
-
 class TicosLedStrip: public TicosDevice {
     public:
         TicosLedStrip(uint8_t nums): m_nums(nums) {}
         uint8_t nums(void) { return m_nums; }
-        void hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t *g, uint32_t *b);
         virtual void fill(uint8_t r, uint8_t g, uint8_t b);
         virtual void fill(TicosRGB888* pixel, uint16_t count=0, bool tail=false);
         virtual void pattern(TicosRGB888* pixels, uint16_t len);
